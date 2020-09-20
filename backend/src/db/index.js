58042b47273d7dcba1b35e71d9f482ac9877f804
Henry Montenegro <hja.montenegro@yavirac.edu.ec>
2020-09-20 09:51:27 -0500

    vale

---
+++ backend/src/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://localhost/red')
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+.then(ok => console.log('db conectada'))
+.catch(err => console.log(err));
+
+
---
+++ frontend/src/app/interfaces/interface.ts
@@ -0,0 +1,45 @@
+export interface Imagen {
+    userId: string;
+    public_id: string;
+    nombre: string;
+    descripcion: string;
+    ruta: string;
+}
+
+export interface User {
+    _id: string;
+    name: string;
+    surname: string;
+    mail: string;
+    acerca: string;
+    password: string;
+    seguidores: number;
+}
+
+export interface Album {
+    _id: string;
+    nombre: string;
+    description: string;
+    user_id: string;
+}
+
+export interface Fotos {
+    _id: string;
+    nombre: string;
+    descripcion: string;
+    albumId: string;
+    userId: string;
+    ruta: string;
+    imagen: File;
+    likes: number;
+    publico: boolean;
+    views: number;
+}
+
+export interface Comentario {
+    _id: string;
+    comentario: string;
+    user_id: string;
+    imagen_id: string;
+    gravatar: string;
+}
